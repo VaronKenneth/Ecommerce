37e9c9c3f4c21638b69f0f46c1fdf7854af6d3f0
Kafazevc <kafazevc@gmail.com>
2024-06-07 18:24:30 -0500

    Changes

--- landing/client/src/app/page.tsx
+++ landing/client/src/app/page.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="relative bg-white p-5 border-2 border-gray-800">
         <p>Este es un div con un borde derecho diagonal.</p>
+      <div className="bg-white flex flex-col items-center justify-center h-[260px] w-full px-10">
+        <div className='w-full flex flex-col items-center justify-center'>
+          <h2 className='text-2xl font-medium p-5 max-[1000px]:text-lg'>Catálogo ESP FILTERS</h2>
+          <div className='w-full flex items-center justify-center relative'><input type="text" className='w-full h-[50px] bg-[#F4F4F4] p-2 rounded max-[1000px]:text-lg' placeholder='Referencia ESP, Aplicación, Tipo de vehículo, OEM..... ' /><div className='absolute right-3 px-2 w-[30px] bg-[#F4F4F4]'>x</div></div>
+          <div className='w-full cursor-pointer'><p>Buscar por medidas</p></div>
+        </div>
+      </div>
+      <div className='w-full h-[600px] flex items-center justify-center bg-[#D4D4D4]'>
+        <Image src='' width={1500} height={1500} alt='banner' className='w-full h-full' />
+      </div>
       </div>
     </main>
   );
